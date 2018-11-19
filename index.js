85fd7afc3c035e1a905864651cbb45cd8dd7c04c
Heidi Engstrom <hengstrom25@gmail.com>
2018-11-19 18:52:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function driversWithRevenueOver(drivers, revenue) {
 
 function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(drivers.revenue) {
-    
+    return drivers.name
   }
 }
